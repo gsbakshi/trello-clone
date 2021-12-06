30f712c58123e4e3ff10bc4e0dbe168d085d05c0
gsbakshi <greaser.bakshi@gmail.com>
2021-12-06 22:01:18 +0530

    Updated section cards

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,56 +1,108 @@
 import { useState } from 'react';
 
+import TextBox from '../text-box/text-box.component';
+import CustomInputBox from '../custom-input-box/custom-input-box.component';
+
+import FloatingButton from '../floating-button/floating-button.component';
+import TaskCard from '../task/task.component';
+
+import Task from '../../utilities/task';
+
 import './card.styles.scss';
 
-const CustomCard = ({ id, deleteCardGroup }) => {
+const CustomCard = ({
+    updateTitle,
+    updateTasks,
+    deleteCardGroup,
+    taskGroup,
+}) => {
     const [isInputTitle, setIsInputTitle] = useState(false);
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState(taskGroup.title);
+    const [tasks, setTasks] = useState([...taskGroup.tasks]);
+    const [task, setTask] = useState('');
 
     const inputTitleHandler = () => setIsInputTitle(!isInputTitle);
 
-    const updateTitle = (event) => {
+    const updateCardTitle = (event) => {
         event.preventDefault();
-        setTitle(event.target.value.trim());
+        const value = event.target.value.trim();
+        setTitle(value);
     };
 
     const submitTitle = (event) => {
         if (event.key === 'Enter') {
             setIsInputTitle(!isInputTitle);
+            updateTitle(taskGroup.id, title);
         }
     };
 
-    const deleteCard = (event) => {
+    const deleteCard = () => {
+        console.log(taskGroup.id, 'deleted');
+        deleteCardGroup(taskGroup.id);
+    };
+
+    const updateTaskInput = (event) => {
         event.preventDefault();
-        console.log(id, 'deleted');
-        deleteCardGroup(id);
+        const value = event.target.value.trim();
+        setTask(value);
+    };
+
+    const onEnter = (event) => {
+        if (event.key === 'Enter') submitTask();
+    };
+
+    const submitTask = () => {
+        const newTask = new Task();
+        newTask.content = task;
+        const tmp = [...taskGroup.tasks];
+        tmp.push(newTask);
+        setTasks([...tmp]);
+        updateTasks(taskGroup.id, tasks);
+        setTask('');
+    };
+
+    const renderCardTitle = () => {
+        return isInputTitle ? (
+            <TextBox
+                placeholder='Enter title'
+                onChange={updateCardTitle}
+                onKeyDown={submitTitle}
+                value={title}
+                autofocus
+                inputType='text'
+            />
+        ) : (
+            <div className='title' onClick={() => inputTitleHandler()}>
+                {taskGroup.title}
+            </div>
+        );
+    };
+
+    const renderTasks = () => {
+        return tasks.length > 0
+            ? tasks.map((task) => (
+                  <TaskCard key={task.id} content={task.content} />
+              ))
+            : 'Add Some Tasks';
     };
 
     return (
         <div className='card-component-group'>
-            <div className='input-card-title'>
-                {isInputTitle ? (
-                    <input
-                        className='input-title'
-                        placeholder='Task Group'
-                        value={title}
-                        onChange={updateTitle}
-                        onKeyPress={submitTitle}
-                    ></input>
-                ) : (
-                    <div className='title' onClick={() => inputTitleHandler()}>
-                        {title || 'Card'}
-                    </div>
-                )}
+            <div className='input-card-title'>{renderCardTitle()}</div>
+            <div className='card-content'>{renderTasks()}</div>
+            <div className='buttons'>
+                <CustomInputBox
+                    input={task}
+                    onInputChange={updateTaskInput}
+                    onEnter={onEnter}
+                    onSubmit={submitTask}
+                />
+                <FloatingButton
+                    onClickHandler={deleteCard}
+                    iconType='fa-times'
+                    alt
+                />
             </div>
-            <input
-                className='input-task'
-                type='text'
-                placeholder='Add Task'
-            ></input>
-            <button className='delete-task'>+</button>
-            <button className='delete-card' onClick={deleteCard}>
-                Delete
-            </button>
         </div>
     );
 };
